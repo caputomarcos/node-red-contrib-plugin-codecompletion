7b8ede6c24bca259777ce5fbad6ad1d21588b1e1
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2024-09-01 14:20:54 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const http = require("http");
 
 module.exports = async function (RED) {
+    const data1 = JSON.stringify({ "name": "granite-code" });
+    const options1 = {
+        hostname: "localhost",
+        port: 11434,
+        path: "/api/pull",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": data1.length
+        }
+    };
+
+    RED.log.info("loading model...");
+    const req1 = http.request(options1, function (res1) {
+        res1.on("data", function (chunk) {
+            let status = JSON.parse(chunk);
+            RED.log.info("downloding model... " + parseInt(100 * status.completed / status.total) + "%");
+        });
+        res1.on("end", function () {
+            RED.log.info("model for codecompletion is loaded");
+            RED.comms.publish('completion', {
+                "message": "model for code completion is loaded"
+            });
+        });
+    });
+
+    req1.on("error", function (error) {
+        RED.log.info("failed to connect to Ollama server");
+        RED.comms.publish('completion', {
+            "message": "failed to connect to Ollama server",
+            "options": { "type": "error" }
+        });
+    });
+    req1.write(data1);
+    req1.end();
+
     RED.httpAdmin.post("/completion", RED.auth.needsPermission("completion.write"), async function (req, res) {
         let code = req.body.code;
         code = code.replace("\nreturn msg;", "");
 
-        const data = JSON.stringify({
+        const data2 = JSON.stringify({
             "model": "granite-code",
             "raw": true,
             "keep_alive": 1800,
@@ -39,19 +75,19 @@ module.exports = async function (RED) {
             "prompt": "<fim_prefix>" + code + "<fim_suffix><fim_middle>"
         });
 
-        const options = {
+        const options2 = {
             hostname: "localhost",
             port: 11434,
             path: "/api/generate",
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "Content-Length": data.length
+                "Content-Length": data2.length
             }
         };
 
         RED.log.info("generating code...");
-        const req2 = http.request(options, function (res2) {
+        const req2 = http.request(options2, function (res2) {
             let completion = "";
             res2.on("data", function (chunk) {
                 completion += JSON.parse(chunk).response;
@@ -63,14 +99,13 @@ module.exports = async function (RED) {
         });
 
         req2.on("error", function (error) {
-            RED.log.info("Failed to connect to Ollama server");
+            RED.log.info("failed to connect to Ollama server");
             RED.comms.publish('completion', {
-                "message": "Failed to connect to Ollama server",
+                "message": "failed to connect to Ollama server",
                 "options": { "type": "error" }
             });
         });
-
-        req2.write(data);
+        req2.write(data2);
         req2.end();
     });
 
